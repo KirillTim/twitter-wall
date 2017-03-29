7444db4f0f6dd2a25d4294224eb8a8cda36e9d85
Kirill <timofeev.kirill.a@gmail.com>
2017-03-29 18:29:15 +0300

    it works!

--- src/main/java/im/kirillt/twitterwall/MainVerticle.kt
+++ src/main/java/im/kirillt/twitterwall/MainVerticle.kt
@@ -1,33 +1,64 @@
 package im.kirillt.twitterwall
 
 import io.vertx.core.AbstractVerticle
+import io.vertx.core.json.JsonObject
 import io.vertx.ext.web.Router
 import io.vertx.ext.web.handler.StaticHandler
+import io.vertx.ext.web.handler.sockjs.BridgeEvent
 import io.vertx.ext.web.handler.sockjs.BridgeEventType
 import io.vertx.ext.web.handler.sockjs.SockJSHandler
 import io.vertx.kotlin.ext.web.handler.sockjs.BridgeOptions
 import io.vertx.kotlin.ext.web.handler.sockjs.PermittedOptions
-import java.text.DateFormat
-import java.time.Instant
-import java.util.*
+import twitter4j.FilterQuery
+import twitter4j.TwitterStream
+import twitter4j.TwitterStreamFactory
+import twitter4j.conf.Configuration
+import twitter4j.conf.ConfigurationBuilder
+import kotlin.collections.HashMap
 
 class MainVerticle : AbstractVerticle() {
-    val PATH_PREFIX = "hashtags:"
+
+    //<set of tags> to map<address, clients count>
+    val requestedTags = hashMapOf<Set<String>, HashMap<String, Int>>()
+    var twitterStream: TwitterStream? = null
 
     override fun start() {
+        twitterStream = TwitterStreamFactory(getTwitterConfig(config())).instance
         val server = vertx.createHttpServer()
         val router = Router.router(vertx)
         val opts = BridgeOptions(outboundPermitted = listOf(PermittedOptions(addressRegex = "$PATH_PREFIX.+")))
 
         val ebHandler = SockJSHandler.create(vertx).bridge(opts, { eventHandler ->
+
             when (eventHandler.type()) {
+
                 BridgeEventType.REGISTER -> {
                     println("register")
-                    println(eventHandler.rawMessage)
+                    val tags = getTags(eventHandler)
+                    val addresses = requestedTags.getOrDefault(tags, hashMapOf())
+                    val eventAddress = eventHandler.rawMessage.getString("address")
+                    println(tags)
+                    //TODO: fix concurrency problems
+                    addresses.put(eventAddress, addresses.getOrDefault(eventAddress, 0) + 1)
+                    requestedTags[tags] = addresses
+                    if (addresses.size == 1) {
+                        twitterStream?.filter(FilterQuery(*requestedTags.keys.flatten().toSet().map { '#' + it }.toTypedArray()))
+                    }
                 }
                 BridgeEventType.UNREGISTER -> {
                     println("unregister")
-                    println(eventHandler.rawMessage)
+                    val tags = getTags(eventHandler)
+                    val addresses = requestedTags.getOrDefault(tags, hashMapOf())
+                    val eventAddress = eventHandler.rawMessage.getString("address")
+                    println(tags)
+                    addresses.put(eventAddress, addresses.getOrDefault(eventAddress, 0) - 1)
+                    if (addresses[eventAddress] == 0) {
+                        addresses.remove(eventAddress)
+                    }
+                    if (addresses.isEmpty()) {
+                        requestedTags.remove(tags)
+                        twitterStream?.filter(FilterQuery(*requestedTags.keys.flatten().toSet().map { '#' + it }.toTypedArray()))
+                    }
                 }
                 else -> {
                 }
@@ -41,18 +72,46 @@ class MainVerticle : AbstractVerticle() {
 
         router.route().handler(staticHandler)
 
-        vertx.createHttpServer().requestHandler(router::accept).listen(8080)
-
-        val eb = vertx.eventBus()
-
-        val tags = listOf("foo", "bar")
-
-        vertx.setPeriodic(5000, {
-            for (tag in tags) {
-                val timestamp = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.MEDIUM).format(Date.from(Instant.now()))
-                eb.publish(PATH_PREFIX + tag, "$timestamp: $tag")
+        val eventBus = vertx.eventBus()
+        twitterStream?.onStatus { status ->
+            val statusTags = status.hashtagEntities.map { it.text }.filterNotNull()
+            println("get: $status")
+            for ((tags, addresses) in requestedTags) {
+                if (statusTags.containsAll(tags)) {
+                    for (address in addresses.keys) {
+                        eventBus.publish(address, status.text)
+                    }
+                }
             }
-            println("periodic")
-        })
+        }
+        twitterStream?.onException { exception ->
+            println(exception)
+        }
+
+        server.requestHandler(router::accept).listen(config().getInteger("port", 8080))
+    }
+
+    companion object {
+        val PATH_PREFIX = "hashtags:"
+
+        fun getTags(event: BridgeEvent): Set<String> {
+            val address = event.rawMessage.getString("address", "")
+            val tags = hashSetOf<String>()
+            if (address.startsWith(PATH_PREFIX)) {
+                tags.addAll(address.drop(PATH_PREFIX.length).split(" ").filter(String::isNotEmpty))
+            }
+            return tags
+        }
+
+        fun getTwitterConfig(globalConfig: JsonObject): Configuration {
+            return ConfigurationBuilder()
+                    .setOAuthConsumerKey(globalConfig.getString("consumerKey"))
+                    .setOAuthConsumerSecret(globalConfig.getString("consumerSecret"))
+                    .setOAuthAccessToken(globalConfig.getString("accessToken"))
+                    .setOAuthAccessTokenSecret(globalConfig.getString("accessTokenSecret"))
+                    .build()
+        }
+
+        data class Tweet(val text: String)
     }
 }
